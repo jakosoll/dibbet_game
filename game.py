cfd4898914f2e92f8f8949c8cd1e893ad9a34204
Evgeiy Krivenko <jako.soll@gmail.com>
2020-09-23 10:56:09 +0300

    Finish game

--- game.py
+++ game.py
@@ -4,24 +4,25 @@ from gibbet import Gibbet
 
 def main():
     while True:
-        print('Приветствуем в игре "Виселица"\n'
-              'Компьютер загадывает случайное слово,\n'
-              'а вы должны его угадать')
-        time.sleep(1)
+        print('Приветствуем в игре "Виселица"!\n'
+              'Компьютер загадывает случайное слово, а вы должны его угадать')
+        time.sleep(3)
         response = input('Готовы начать? Введите "Y" для начала или любой символ для выхода: ')
         time.sleep(1)
         if response.upper() != 'Y':
             print('До свидания!')
             time.sleep(1)
             break
-        time.sleep(1)
         print('Переключите клавиатуру на русскую раскладку, игра начинается...')
         time.sleep(1)
         game = Gibbet()
         game.generate_word()
         while game.run_game:
-            game.take_letter(input('Попробуйте угадать букву: '))
-        response = input('Желатете сыграть еще раз?Введите "Y" для начала или любой символ для выхода: ')
+            time.sleep(1)
+            game.take_char(input('Попробуйте угадать букву: ', ))
+        # if not game.win:
+        #     game.get_right_word()
+        response = input('Желатете сыграть еще раз? Введите "Y" для начала или любой символ для выхода: ')
         if response.upper() != 'Y':
             print('До свидания!')
             break
--- gibbet.py
+++ gibbet.py
@@ -1,12 +1,13 @@
 import random
 from typing import List
+import time
 
 
 class Gibbet:
     _word: str
     right_chars: List[str]
     error_chars: List[str]
-    end_game: bool
+    guess_word: str
 
     def __init__(self, errors=5):
         self.errors = errors
@@ -14,49 +15,74 @@ class Gibbet:
         self.error_chars = []
         self.run_game = True
         self.win = None
+        self.guess_word = ''
 
     def generate_word(self):
         """Генерация слова, пока реализуем заглушку"""
-        with open('WordsStockRus.txt', 'r') as f:
+        with open('WordsStockRus.txt', 'r', encoding='utf-8') as f:
             word_list = []
             for line in f.readlines():
                 word_list.append(line.strip())
             self._word = random.choice(word_list)
             self.errors = len(self._word) + 1
+        time.sleep(1)
         print('Компьютер сгенерировал слово')
         self.print_try_amount()
 
-    def take_letter(self, char):
-        """Ввести букву (попытка)"""
+    def take_char(self, char: str):
+        """
+        Ввести букву
+        Основная логика
+        """
+        if not self._check_char_lenght(char):
+            time.sleep(1)
+            print('Ошибка, вы ввели больше одного символа')
+            return
+        if char in self.right_chars or char in self.error_chars:
+            print(f'Вы уже вводили {char}, введите другую букву')
+            time.sleep(1)
+            self.print_word()
+            return
         if self._check_letter_in_word(char):
             print('Bingo!\n')
+            print(f'Вы угадали букву {char}')
             self.right_chars.append(char)
             self.print_word()
+            self._check_game()
         else:
             print('Извините, вы ошиблись')
             self.errors -= 1
+            self._check_game()
+            if not self.run_game:
+                return
             self.error_chars.append(char)
+            print(f'Вы вводили следушие буквы: \n{self.show_letters()}')
+            self.print_word()
             self.print_try_amount()
+            self._check_game()
 
     def print_try_amount(self):
         """Кол-во оставшихся попыток"""
+        time.sleep(1)
         print(f"Осталось попыток: {self.errors} шт.")
+        time.sleep(1)
 
     def print_word(self):
         """Показать открытые буквы,
         если буква скрыта вместо неё ставим подчёркивание или дефис"""
-        word: str = ''
+        self.guess_word = ''
         for char in self._word:
             if char in self.right_chars:
-                word += char
+                self.guess_word += char
             else:
-                word += '-'
-        print(word)
+                self.guess_word += '-'
+        time.sleep(1)
+        print(f'Отгадываемое слово: => {self.guess_word}')
 
     def show_letters(self):
         """Показать буквы, которые клиент вводил"""
-
-        return ', '.join(self.error_chars)  # TODO: огранизовть проверку и вывод введеных букв
+        time.sleep(1)
+        return ', '.join(self.error_chars)
 
     def _check_letter_in_word(self, char: str):
         if self._word:
@@ -65,8 +91,15 @@ class Gibbet:
     def _check_game(self):
         if self.errors <= 0:
             self.run_game = False
-            print('Извините, вы проиграли')
-        elif len(self.right_chars) == len(self._word):
+            print('Вы проиграли')
+            self._get_right_word()
+        elif self.guess_word == self._word:
             self.run_game = False
             self.win = True
             print('Поздравляем, вы выиграли')
+
+    def _get_right_word(self):
+        print(f'Загаданное слово: "{self._word}"')
+
+    def _check_char_lenght(self, char):
+        return len(char) == 1
